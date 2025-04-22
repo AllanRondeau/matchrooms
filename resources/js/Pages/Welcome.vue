b115dc5bebdab22889d5d29a1ad82bc22467aac2
allan <arondeau@wemanity.com>
2025-04-22 17:09:09 +0200

    init

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,23 @@
+<template>
+    <Head>
+        <title>Home</title>
+    </Head>
+
+    <div class="p-6">
+        <h1 class="text-2xl font-bold">Hello Inertia + Vue3 🎉</h1>
+        <p class="mt-4">{{ message }}</p>
+<!--        <Link href="/about" class="text-blue-600 underline">Go to About</Link>-->
+    </div>
+</template>
+
+<script setup>
+import {Head, Link} from '@inertiajs/inertia-vue3'
+
+const props = defineProps({
+    message: {
+        type: String,
+        required: true,
+    },
+})
+</script>
+
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './resources/js/**/*.vue',
+        './resources/js/**/*.js',
+        './resources/views/**/*.blade.php',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
